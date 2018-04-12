b10fccc73eb979c4530a058d496d1801c9eff2f5
Gren Drake <gren@grenslair.com>
2018-04-12 02:15:57 -0700

    Adds list of known labels

--- assem_build.c
+++ assem_build.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "assemble.h"
@@ -11,6 +12,10 @@ struct label_def {
 
 static void skip_line(struct token **current);
 static void parse_error(struct token *where, const char *err_msg);
+static int add_label(struct label_def **first_lbl, const char *name, int pos);
+static void dump_labels(struct label_def *first);
+static void free_labels(struct label_def *first);
+
 
 static void skip_line(struct token **current) {
     struct token *here = *current;
@@ -34,10 +39,48 @@ static void parse_error(struct token *where, const char *err_msg) {
            err_msg);
 }
 
+static int add_label(struct label_def **first_lbl, const char *name, int pos) {
+    struct label_def *cur = *first_lbl;
+
+    struct label_def *new_lbl = malloc(sizeof(struct label_def));
+    if (!new_lbl) {
+        return 0;
+    }
+    new_lbl->name = str_dup(name);
+    new_lbl->pos = pos;
+
+    if (cur == NULL) {
+        new_lbl->next = NULL;
+    } else {
+        new_lbl->next = cur;
+    }
+    *first_lbl = new_lbl;
+    return 1;
+}
+
+static void dump_labels(struct label_def *first) {
+    struct label_def *cur = first;
+    while (cur) {
+        printf("0x%08X  %s\n", cur->pos, cur->name);
+        cur = cur->next;
+    }
+}
+
+static void free_labels(struct label_def *first) {
+    struct label_def *cur = first;
+    while (cur) {
+        struct label_def *next = cur->next;
+        free(cur->name);
+        free(cur);
+        cur = next;
+    }
+}
+
 int parse_tokens(struct token_list *list) {
     int has_errors = 0;
     int code_pos = 0;
     int done_initial = 0;
+    struct label_def *first_lbl = NULL;
 
     struct token *here = list->first;
     while (here) {
@@ -67,7 +110,7 @@ int parse_tokens(struct token_list *list) {
         done_initial = 1;
 
         if (here->next && here->next->type == tt_colon) {
-            printf("LBL %s @ %d\n", here->text, code_pos);
+            add_label(&first_lbl, here->text, code_pos);
             here = here->next->next;
             continue;
         }
@@ -77,5 +120,7 @@ int parse_tokens(struct token_list *list) {
         skip_line(&here);
     }
 
+    dump_labels(first_lbl);
+    free_labels(first_lbl);
     return !has_errors;
 }
